6a8f07425a807692eda02a587a4d8f3f585cce65
GauravFrontend <gaurav9911093928@gmail.com>
2024-02-28 17:35:57 +0530

    project

---
+++ index.js
@@ -0,0 +1,27 @@
+// alert("working")
+
+
+function openPopup() {
+
+    var popupUrl = "popup.html";
+    
+
+    var popupWidth = 500;
+    var popupHeight = 600;
+
+
+    var leftPosition = (window.screen.width - popupWidth) / 2;
+    var topPosition = (window.screen.height - popupHeight) / 2;
+
+
+    var popupOptions = "width=" + popupWidth + ",height=" + popupHeight + ",top=" + topPosition + ",left=" + leftPosition;
+
+
+    window.open(popupUrl, "_blank", popupOptions);
+}
+
+
+var popupButton = document.getElementById("popupButton");
+
+
+popupButton.addEventListener("click", openPopup);
